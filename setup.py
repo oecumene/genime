2deca8b8e68657e166c119337afebac661f57ef1
Serge <kims230599@gmail.com>
2020-09-05 00:24:24 +0300

    data example + setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,30 @@ from setuptools import setup
 
 
 def main():
-    return 0
+    package_name = "genime"
+    package_path = Path(__file__).parent.absolute()
+    print(package_path)
+
+    packages = {
+        str(p.parent.relative_to(package_path)).replace('/', '.')
+        for p in (package_path / package_name).rglob("__init__.py")
+    }
+
+    setuptools.setup(
+        name=package_name,
+        version="0.0.1",
+        description="Generative Anime",
+        packages=packages,
+        package_dir={package_name: package_name},
+        include_package_data=True,
+        package_data={package_name: ["genime/data"]},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
 
 
 if __name__ == '__main__':
